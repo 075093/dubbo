4e8eb3f61f404c405999f1bd032cf432e2bd4ba2
wangzf075093@yimidida.com <wangzf075093@yimidida.com>
2019-03-21 14:35:16 +0800

    zk伪集群

--- dubbo-interface/src/main/java/com/wzf/dubbo/service/DemoController.java
+++ dubbo-interface/src/main/java/com/wzf/dubbo/service/DemoController.java
@@ -9,14 +9,14 @@ import com.alibaba.dubbo.config.annotation.Reference;
 @RestController
 public class DemoController {
 
-	/*
-	 * @Reference private UserService userService;
-	 */
+	@Reference
+	private UserService userService;
+
 	@Reference
 	private DemoService demoService;
 
 	@RequestMapping(value = "/xxx", method = RequestMethod.GET)
 	public String getdubbo() {
-		return /* userService.getUser() + */demoService.Test() + demoService.Test1(null) + demoService.Test2(null);
+		return userService.getUser() + demoService.Test() + demoService.Test1(null) + demoService.Test2(null);
 	}
 }
